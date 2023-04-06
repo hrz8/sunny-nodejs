0c0c1b984138b886776f819ae4f14eeb6ccecaa2
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-04-06 21:50:48 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// const { print, greeting } = require('./modules/simple/mymodule');
+const lodash = require('lodash');
+
+
+console.log("hello, chapter 5");
+console.log("hello, chapter 6");
+console.log("hello, chapter 8");
+console.log("hello, chapter 8");
+console.log("hello, chapter 8");
+// print();
+// greeting();
