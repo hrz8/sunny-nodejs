0b8efc5e0e822a7f7631a334d72c2289f8dbd0bd
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-04-15 09:19:35 +0700

    add ejs

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ const PORT_EXPRESS = 4001;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.json()); // application level middleware/third party middleware
 app.use(appLevelMiddlewareBefore);
 
+app.get('/test-ejs/:name', function(req, res) {
+    res.render('index.ejs', {
+        name: req.params.name
+    });
+});
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/articles', articlesRouter);
 app.use(function(req, res) {
